4e9026a371cdd4b7f4a8c295ac24eaf4a279673b
Ian Gruenig <ian.gruenig@gmail.com>
2022-04-28 13:12:03 +0200

    Remove unused files

---
+++ NewsTests/BookmarkControllerTests.swift
@@ -0,0 +1,24 @@
+//
+//  BookmarkControllerTests.swift
+//  NewsTests
+//
+//  Created by Ian Grünig on 28.04.22.
+//
+
+import XCTest
+
+class BookmarkControllerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testFindAll_Nil_ReturnsEmptyArray() throws {
+        
+    }
+
+}
