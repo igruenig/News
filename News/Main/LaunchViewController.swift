406cc52183ba6577623efe34cc08210449f9bf31
Ian Gruenig <ian.gruenig@gmail.com>
2022-04-28 16:00:35 +0200

    Add splash screen animation

---
+++ News/Main/LaunchViewController.swift
@@ -0,0 +1,59 @@
+//
+//  LaunchViewController.swift
+//  News
+//
+//  Created by Ian Grünig on 28.04.22.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let imageView = UIImageView(image: UIImage(named: "logo"))
+        imageView.frame = CGRect(x: 0, y: 0, width: 280, height: 280)
+        view.addSubview(imageView)
+        
+        self.imageView = imageView
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        imageView.center = view.center
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let navVC = ViewControllerFactory.makeViewController(identifier: "NavigationViewController") as! NavigationViewController
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+1.5, execute: { [weak self] in
+            self?.animate(vc: navVC)
+        })
+    }
+    
+    private func animate(vc: NavigationViewController) {
+        UIView.animate(withDuration: 0.8, animations: { [weak self] in
+            guard let this = self else {
+                return
+            }
+            let size = this.view.frame.width * 3
+            let diffX  = size - this.view.frame.size.width
+            let diffY = this.view.frame.size.height - size
+            
+            this.imageView.frame = CGRect(x: -(diffX/2.0), y: diffY/2.0, width: size, height: size)
+            this.imageView.alpha = 0.0
+        })
+        
+        DispatchQueue.main.asyncAfter(deadline: .now(), execute: { [weak self] in
+            vc.modalTransitionStyle = .crossDissolve
+            vc.modalPresentationStyle = .fullScreen
+            self?.present(vc, animated: true)
+        })
+    }
+
+}
